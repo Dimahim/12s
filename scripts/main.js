d7e30cb7ba40d08a7af42701666325685c7e2102
Dimahim <alhimovd877@yandex.ru>
2022-09-20 19:53:52 +0300

    Curs

--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,4 @@
-// // cоздаем массив__________________________________________
-// const initialCards = [
-//   {
-//     link: './images/kirill-pershin-1088404.jpg',
-//     name: 'Карачаево-Черкесия'
-//   },
-//   {
-//     link: './images/kirill-pershin-1404681-unsplash.png',
-//     name: 'Гора Эльбрус'
-//   },
-//   {
-//     link: './images/kirill-pershin-1556355-unsplash.jpg',
-//     name: 'Домбай'
-//   },
-//   {
-//     link: './images/photo-grid-baikal-2.jpg',
-//     name: 'Байкал'
-//   },
-//   {
-//     link: './images/photo-grid-sochi.jpg',
-//     name: 'Сочи'
-//   },
-//   {
-//     link: './images/place-karelia.jpg',
-//     name: 'Карелия'
-//   },
-// ];
-// const profilePopupOpenButton = document.querySelector('.profile__edit-button');
-// const profTitle = document.querySelector('.profile__title');
-// const profSubtitle = document.querySelector('.profile__subtitle');
-// const profilePopup = document.querySelector('.popup_profile_editing');
-// const profileСlosePopup = document.querySelector('.popup__close-icon_profile_edit');
-// const nameInput = document.querySelector('.form__field_item_name');
-// const jobInput = document.querySelector('.form__field_item_job');
-// const profileFormPopup = document.querySelector('.form_profile_edit');
-// // переменные для попапа добавление карточек
-// const popupAddCards = document.querySelector('.popup_container_cards');
-// const buttonAddCards = document.querySelector('.profile__add-button');
-// const cardsClosePopup = document.querySelector('.popup__close-icon_close_cards');
-// const formCardsPopup = document.querySelector('.popup__form_add_cards');
-// const imputTitleCards = document.querySelector('.form__field_item_title');
-// const inputLinkCards = document.querySelector('.form__field_item_link');
-// const usersOnline = document.querySelector('.template-container');
-// const popupClosCards = document.querySelector('.popup_content_image');
-// const imageCardsmage = document.querySelector('.popup__image');
-// const imagePopup = document.querySelector('.template-container');
-// const buttnClosePopupCards = document.querySelector('.popup__close-icon_close_image');
-// const figcaption = document.querySelector('.popup__image-caption');
-// const userTemplate = document.querySelector('.elements');
-
-// // Функция для открытия попапа 
+ // // Функция для открытия попапа 
 // function openPopup (popup) {
 //   popup.classList.add('popup_opened');
   
@@ -206,7 +156,6 @@ function closePopupCode() {
 }
 //форма ввода
 buttonInput.addEventListener('click', closePopupCode);
-
 
 // функция открытия или закрытия меню
 function openedMenu() {
